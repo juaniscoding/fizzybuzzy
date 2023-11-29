ff8d5c9a5e43226f873b5031ccb2ad82c7070bd3
juaniscoding <149956202+juaniscoding@users.noreply.github.com>
2023-11-29 17:36:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a numerical value");
+        double value = in.nextDouble();
+
+        if (value % 3 == 0&& value%5==0){
+            System.out.println("FIZZBUZZ");
+        }
+        else if (value%3==0){
+            System.out.println("FIZZ");
+        } else if (value%5==0) {
+            System.out.println("BUZZ");
+        }else{
+            System.out.println(value);
+        }
+    }
+}
